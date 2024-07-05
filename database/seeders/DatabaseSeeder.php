d664b67e795e4b92ae4559433bbd3c6060554fd9
Wesleyotio <wesleymagnos@gmail.com>
2024-07-05 13:04:01 -0300

    created seeders for users and addresses

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Address;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        User::factory()
+            ->has(Address::factory()->count(1))
+            ->count(10)
+            ->create();
     }
 }

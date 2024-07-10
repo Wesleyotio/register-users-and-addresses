5c34650ffa471c477f146f8e7307ad11e8441e37
Wesleyotio <wesleymagnos@gmail.com>
2024-07-10 08:55:26 -0300

    correction exclusion from table

--- database/migrations/2024_07_04_202749_create_addresses_table.php
+++ database/migrations/2024_07_04_202749_create_addresses_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('stateAbbr');
             $table->string('country');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
